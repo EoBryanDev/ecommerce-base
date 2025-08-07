46eca1a5413127fb105c8ed22a3d6d807e140305
Bryan <mauriciobryan1998@gmail.com>
2025-08-07 14:04:44 -0300

    ensure cart shipping address can be selected

--- src/app/cart/identification/page.tsx
+++ src/app/cart/identification/page.tsx
@@ -16,23 +16,35 @@ const IdentificationPage = async () => {
   if (!session?.user.id) {
     redirect("/");
   }
-  const shippingAddresses = await db.query.shippingAddressTable.findMany({
-    where: eq(shippingAddressTable.userId, session.user.id),
-  });
   const cart = await db.query.cartTable.findFirst({
-    where: eq(cartTable.userId, session.user.id),
+    where: (cart, { eq }) => eq(cart.userId, session.user.id),
     with: {
-      items: true,
+      shippingAddress: true,
+      items: {
+        with: {
+          productVariant: {
+            with: {
+              product: true,
+            },
+          },
+        },
+      },
     },
   });
   if (!cart || cart?.items.length === 0) {
     redirect("/");
   }
+  const shippingAddresses = await db.query.shippingAddressTable.findMany({
+    where: eq(shippingAddressTable.userId, session.user.id),
+  });
   return (
     <>
       <Header />
       <div className="px-5">
-        <Addresses shippingAddresses={shippingAddresses} />
+        <Addresses
+          shippingAddresses={shippingAddresses}
+          defaultShippingAddressId={cart.shippingAddress?.id || null}
+        />
       </div>
     </>
   );
--- src/hooks/queries/use-cart.ts
+++ src/hooks/queries/use-cart.ts
@@ -1,14 +1,15 @@
 import { useQuery } from "@tanstack/react-query";
 
-import { getCart } from "@/actions/add-cart-product/get-cart";
-
-
+import { getCart } from "@/actions/get-cart";
 
 export const getUseCartQueryKey = () => ["cart"] as const;
 
-export const useCart = () => {
+export const useCart = (params?: {
+    initialData?: Awaited<ReturnType<typeof getCart>>;
+}) => {
     return useQuery({
         queryKey: getUseCartQueryKey(),
         queryFn: () => getCart(),
+        initialData: params?.initialData,
     });
 };
