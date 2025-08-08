8a002f1242ec721471d418134b369f61ee71ae46
Bryan <mauriciobryan1998@gmail.com>
2025-08-08 12:46:57 -0300

    feat: ensure stripe session is created

--- src/app/cart/confirmation/components/finish-order-button.tsx
+++ src/app/cart/confirmation/components/finish-order-button.tsx
@@ -1,9 +1,11 @@
 "use client";
+import { loadStripe } from "@stripe/stripe-js";
 import { Loader2 } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
 
+import { createCheckoutSession } from "@/actions/create-checkout-session";
 import { Button } from "@/components/ui/button";
 import {
   Dialog,
@@ -16,8 +18,19 @@ import { useFinishOrder } from "@/hooks/mutations/use-finish-order";
 const FinishOrderButton = () => {
   const [successDialogIsOpen, setSuccessDialogIsOpen] = useState(false);
   const finishOrderMutation = useFinishOrder();
-  const handleFinishOrder = () => {
-    finishOrderMutation.mutate();
+  const handleFinishOrder = async () => {
+    if (!process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY) {
+      throw new Error("Stripe publishable key is not set");
+    }
+    const { orderId } = await finishOrderMutation.mutateAsync();
+    const checkoutSession = await createCheckoutSession({ orderId });
+    const stripe = await loadStripe(
+      process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY,
+    );
+    if (!stripe) {
+      throw new Error("Failed to load Stripe");
+    }
+    await stripe.redirectToCheckout({ sessionId: checkoutSession.id });
     setSuccessDialogIsOpen(true);
   };
   return (
@@ -53,6 +66,7 @@ const FinishOrderButton = () => {
             na seção de “Meus Pedidos”.{" "}
           </DialogDescription>{" "}
           <DialogFooter>
+            {" "}
             <Button className="rounded-full" size="lg">
               {" "}
               Ver meus pedidos{" "}
@@ -63,7 +77,8 @@ const FinishOrderButton = () => {
               size="lg"
               asChild
             >
-              <Link href="/">Voltar para a loja</Link>
+              {" "}
+              <Link href="/">Voltar para a loja</Link>{" "}
             </Button>{" "}
           </DialogFooter>{" "}
         </DialogContent>{" "}
